46fcbbaebd8c859de1007e556bee6a9e442ed8e0
68018820+AlexeySobolevskiy@users.noreply.github.com <68018820+AlexeySobolevskiy@users.noreply.github.com>
2022-07-07 00:25:04 +0400

    Initial Commit

---
+++ WeatherAppOnSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAppOnSwiftUI
+//
+//  Created by Алексей Соболевский on 7.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAppOnSwiftUI/WeatherAppOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppOnSwiftUIApp.swift
+//  WeatherAppOnSwiftUI
+//
+//  Created by Алексей Соболевский on 7.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
