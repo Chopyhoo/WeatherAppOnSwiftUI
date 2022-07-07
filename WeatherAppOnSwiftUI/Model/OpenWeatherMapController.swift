5c77503ef7fc5a66f60e4c3a89347779b3de6e64
68018820+AlexeySobolevskiy@users.noreply.github.com <68018820+AlexeySobolevskiy@users.noreply.github.com>
2022-07-07 23:05:12 +0400

    Add models and request

--- WeatherAppOnSwiftUI/Model/OpenWeatherMapController.swift
+++ WeatherAppOnSwiftUI/Model/OpenWeatherMapController.swift
@@ -20,7 +20,37 @@ final class OpenWeatherMapController: WebServiceController {
                   completionHandler(nil, .invalidURL(urlString: endpoint))
                   return
               }
+        
+        let dataTask = URLSession.shared.dataTask(with: endpointURL) { data, response, error in
+            if let error = error {
+                completionHandler(nil, .forwarded(error))
+                return
+            }
+            guard let responseData = data else {
+                completionHandler(nil, .invalidPayload(fetchURL: endpointURL))
+                return
+            }
+            
+            // decode json
+            let decoder = JSONDecoder()
+            do {
+                let weatherList = try decoder.decode(OpenWeatherMapContainer.self, from: responseData)
+                guard let weatherInfo = weatherList.list?.first,
+                      let weather = weatherInfo.weather.first?.main,
+                      let temperature = weatherInfo.main.temp else {
+                          completionHandler(nil, .invalidPayload(fetchURL: endpointURL))
+                          return
+                      }
+                
+                // compose weather information
+                let weatherDescription = "\(weather) \(temperature) °F"
+                completionHandler(weatherDescription, nil)
+            } catch let error {
+                completionHandler(nil, .forwarded(error))
+            }
+            
+        }
+    
+        dataTask.resume()
     }
-    
-    
 }
